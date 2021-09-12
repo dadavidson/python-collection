69b081b1084f400a571e242e7af7f59a1f8b3740
Daniel Davidson <r3dd3ath@gmail.com>
2021-09-11 23:07:00 -0500

    update

---
+++ code/helloworld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Print string
+print("Hello, world!")                  # Double quotes
+print('\n')                             # Print a newline
+print('Hello, world!')                  # Single quotes
+
+print("""This string runs
+multiple lines!""")                     # Triple quotes for for multiple lines
+
+print("This string is " + "awesome!")   # String concatenation
---
+++ code/math.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Math
+print(50 + 50)                  # Addition
+print(50 - 50)                  # Subtraction
+print(50 * 50)                  # Multiplication
+print(50 / 50)                  # Division
+
+print(50 + 50 - 50 * 50 / 50)   # PEMDAS
+print(50 ** 2)                  # Exponents
+print(50 % 6)                   # Modulus
+print(50 / 6)                   # Float Division
+print(50 // 6)                  # Flat Division
