64283a922860e2f5fa743883d8ec657b9635a7fe
Daniel Davidson <r3dd3ath@gmail.com>
2021-09-21 04:16:00 -0500

    Create modules.py

---
+++ code/modules.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Modules
+
+import sys                              # importing module
+from datetime import datetime as dt     # importing module as alias
+
+print(dt.now())
