0ca9e66940333d9ca5ce3787e9dbd8886c9fd1b7
Daniel Davidson <r3dd3ath@gmail.com>
2021-09-21 03:19:31 -0500

    Create looping.py

---
+++ code/looping.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Looping
+
+# For loops - start to finish of an iterate
+
+vegetables = ["cucumber", "spinach", "cabage"]
+
+for x in vegetables:
+    print(x)
+
+
+# While loops - Execute as long as True
+
+i = 1
+
+while i < 10:
+    print(i)
+    i += 1
