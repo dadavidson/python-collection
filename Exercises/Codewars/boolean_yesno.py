135e786c69281fbc02c8ce77b5f0bfe46e1f4966
Daniel Davidson <r3dd3ath@gmail.com>
2022-05-19 16:58:02 -0500

    commit

---
+++ Exercises/Codewars/boolean_yesno.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Convert boolean values to strings 'Yes' or 'No'
+"""
+
+__author__ = "Daniel Davidson"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    """ Main entry point of the app """
+    class Boolean(object):
+        def bool_to_word(bvalue):
+            if bvalue == 'True' or bvalue == 'true':
+                bvalue = True
+                return 'Yes'
+            if bvalue == 'False' or bvalue == 'false':
+                bvalue = False
+                return 'No'
+            else:
+                return 'Error'
+    bInput = input("Enter a boolean value: ")
+    bOutput = Boolean.bool_to_word(bInput)
+    print(bOutput)
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
