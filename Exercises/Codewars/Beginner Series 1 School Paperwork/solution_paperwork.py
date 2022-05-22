363ddccbc1606fa0f9ddf0134643152912acc215
Daniel Davidson <r3dd3ath@gmail.com>
2022-05-21 22:48:13 -0500

    Update solution_paperwork.py

---
+++ Exercises/Codewars/Beginner Series 1 School Paperwork/solution_paperwork.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Your classmates asked you to copy some paperwork for them.
+You know that there are 'n' classmates and the paperwork has 'm' pages.
+"""
+
+__author__ = "Daniel Davidson"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def paperwork(n, m):
+    # Happy Coding! ^_^
+    if n < 0 or m < 0:
+        f = 0
+    else:
+        f = n * m
+    return f
+
+
+output = paperwork(5, 5)
+print(output)
