98ebbf12d9a8febf055b9b0dc50c04ca286e2ec4
Daniel Davidson <r3dd3ath@gmail.com>
2022-05-22 23:54:53 -0500

    Create banjo.py

---
+++ Exercises/Codewars/Are You Playing Banjo/banjo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or "r", you are playing banjo!
+"""
+
+__author__ = "Daniel Davidson"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def are_you_playing_banjo(name):
+    # Implement me!
+    name = str.capitalize(name)
+    if name[0] == 'R':
+        answer = name + " plays banjo"
+    else:
+        answer = name + " does not play banjo"
+    return answer
+
+
+output = are_you_playing_banjo(input("What is your name? "))
+print(output)
