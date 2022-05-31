039fd951225c0159f98842cd11ab6f7593cdadf4
Daniel Davidson <r3dd3ath@gmail.com>
2022-05-31 08:17:10 -0500

    update square_sum.py

--- Exercises/Codewars/Square Sum/square_sum.py
+++ Exercises/Codewars/Square Sum/square_sum.py
@@ -1,3 +1,23 @@
-def square_sum(numbers):
+#!/usr/bin/env python3
+"""
+# Instructions
+Complete the square sum function so that it squares
+each number passed into it and then sums the results together.
+"""
+
+__author__ = "Daniel Davidson"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def square_sum(*numbers):
     # your code here
-    pass
+    results = []
+    for n in numbers:
+        results.append(n ** 2)
+
+    return sum(results)
+
+
+output = square_sum(0, 3, 4, 5)
+print(output)
