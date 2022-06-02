b58944d4d9335932df2084d51e3d67fd03fc0f45
Daniel Davidson <r3dd3ath@gmail.com>
2022-06-02 08:24:25 -0500

    Commit convert_num_to_str.py

---
+++ Exercises/Codewars/Convert a Number to a String/convert_num_to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+# Instructions
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+"""
+
+__author__ = "Daniel Davidson"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def number_to_string(n):
+    return str(n)                           # Convert input integer -> string
+
+
+output = number_to_string(348597)           # Input random number
+print(output)
+print(type(output))
