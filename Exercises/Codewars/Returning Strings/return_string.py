d007726a9e0583089f1ed308790ef1300172d414
Daniel Davidson <r3dd3ath@gmail.com>
2022-06-07 08:47:47 -0500

    Commit return_string.py

---
+++ Exercises/Codewars/Returning Strings/return_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+# Instructions
+Make a function that will return a greeting statement that uses an input;
+your program should return, "Hello, <name> how are you doing today?".
+"""
+
+__author__ = "Daniel Davidson"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def greet(name):
+    return f'Hello, {name} how are you doing today?'
