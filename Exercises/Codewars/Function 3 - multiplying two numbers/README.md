00dace61fe099a0a3ddc486686c9b5fa6fd22ff9
Daniel Davidson <r3dd3ath@gmail.com>
2022-06-16 22:52:36 -0500

    Commit Codewars

---
+++ Exercises/Codewars/Function 3 - multiplying two numbers/README.md
@@ -0,0 +1,5 @@
+# 8 kyu - Function 3 - multiplying two numbers
+---
+
+## Instructions
+Implement a function which multiplies two numbers.
---
+++ Exercises/Codewars/Function 3 - multiplying two numbers/multiply_func.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+# Instructions
+Create a function which translates a given DNA string into RNA.
+"""
+
+__author__ = "Daniel Davidson"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+output = multiply(2, 3)
+print(output)
